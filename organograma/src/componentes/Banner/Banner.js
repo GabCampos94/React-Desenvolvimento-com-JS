71925ff895afed3efb798263df9a8c0060e703aa
Gabriel Campos <gabriel__2336@hotmail.com>
2023-04-12 16:08:54 -0300

    Configuração do Banner

---
+++ organograma/src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css"
+
+function Banner() {
+    return(
+       <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner Principal da página"></img>
+       </header>
+        
+    )
+}
+
+export default Banner
